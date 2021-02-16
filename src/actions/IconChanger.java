e90a59df3e28d14c11e6b1d96278e0025d47601f
Pavel <rengetsu@mail.ru>
2021-02-16 22:46:20 +0200

    Create IconChanger Class

--- src/actions/IconChanger.java
+++ src/actions/IconChanger.java
@@ -27,10 +27,34 @@ public class IconChanger {
     public void changeDownMenuButtonIcon(Path currentDir, String buttonName, String iconName)
     {
         if      (   "Mode".equals(buttonName)       ){
-            Interface.downMenu_ModeButton.setIcon(    new ImageIcon(currentDir + "\\src\\images\\" + iconName));
+            Interface.downMenu_ModeButton.setIcon(          new ImageIcon(currentDir + "\\src\\images\\" + iconName));
         }
         else if (   "Pattern".equals(buttonName)    ){
-            Interface.downMenu_PatternButton.setIcon(    new ImageIcon(currentDir + "\\src\\images\\" + iconName));
+            Interface.downMenu_PatternButton.setIcon(       new ImageIcon(currentDir + "\\src\\images\\" + iconName));
+        }
+        else if (   "Display".equals(buttonName)    ){
+            Interface.downMenu_DisplayButton.setIcon(       new ImageIcon(currentDir + "\\src\\images\\" + iconName));
+        }
+        else if (   "Trigger".equals(buttonName)    ){
+            Interface.downMenu_TriggerButton.setIcon(       new ImageIcon(currentDir + "\\src\\images\\" + iconName));
+        }
+        else if (   "Inputs".equals(buttonName)    ){
+            Interface.downMenu_InputsButton.setIcon(        new ImageIcon(currentDir + "\\src\\images\\" + iconName));
+        }
+        else if (   "Calibration".equals(buttonName)    ){
+            Interface.downMenu_CalibrationButton.setIcon(   new ImageIcon(currentDir + "\\src\\images\\" + iconName));
+        }
+        else if (   "Timing".equals(buttonName)    ){
+            Interface.downMenu_TimingButton.setIcon(       new ImageIcon(currentDir + "\\src\\images\\" + iconName));
+        }
+        else if (   "Levels".equals(buttonName)    ){
+            Interface.downMenu_LevelsButton.setIcon(        new ImageIcon(currentDir + "\\src\\images\\" + iconName));
+        }
+        else if (   "Outputs".equals(buttonName)    ){
+            Interface.downMenu_OutputsButton.setIcon(       new ImageIcon(currentDir + "\\src\\images\\" + iconName));
+        }
+        else if (   "Utility".equals(buttonName)    ){
+            Interface.downMenu_UtilityButton.setIcon(       new ImageIcon(currentDir + "\\src\\images\\" + iconName));
         }
     }
 }
